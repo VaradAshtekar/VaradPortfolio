22d645b7f061e155627e47d06c371044d010d03b
VaradAshtekar <66002654+VaradAshtekar@users.noreply.github.com>
2021-07-31 21:47:26 +0530

    change2

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar() {
         <div>
             <nav className="navbar sticky-top navbar-expand-lg navbar-custom">
                 <div className="container-fluid">
-                <a href="./"><img src={logo} alt="" width="35" height="35"/></a>
+                <a href="/"><img src={logo} alt="" width="35" height="35"/></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon" ></span>
                     </button>
@@ -17,13 +17,13 @@ export default function Navbar() {
 
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="./">Home</a>
+                                <a className="nav-link active" aria-current="page" href="/">Home</a>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="./about">About Me</Link>
+                                <Link className="nav-link" to="/about">About Me</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="./contact">Contact</Link>
+                                <Link className="nav-link" to="/contact">Contact</Link>
                             </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
